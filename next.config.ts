03b319a76e19b493c4f99411a456bcc638082407
tarbipyakurel21 <tarbipyakurel21@gmail.com>
2025-11-24 21:42:15 -0600

    Fix deploy script and add basepath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/inventory-app",
+  assetPrefix: "/inventory-app/",
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
   reactCompiler: true,
 };
